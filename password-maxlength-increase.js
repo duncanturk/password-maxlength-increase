1314b03210c1aab4bc4fff63c11e9a01a8f5f02d
duncanturk <31703168+duncanturk@users.noreply.github.com>
2018-10-07 20:28:48 +0200

    execute on ready

--- password-maxlength-increase.js
+++ password-maxlength-increase.js
@@ -4,4 +4,8 @@
 // @require https://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
-$('input[type="password"][maxlength]').removeAttribute("maxlength");
+$(document).ready(function(){
+    let limitedPasswordFields = $('input[type="password"][maxlength]');
+    if(limitedPasswordFields.length > 0)
+        limitedPasswordFields.removeAttr("maxlength");
+});
